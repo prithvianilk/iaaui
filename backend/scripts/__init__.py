c247aff02bb7e891ed68bd15071cae116c165776
compspandan <compspandan@gmail.com>
2023-03-25 22:13:15 +0530

    create lb

--- backend/scripts/__init__.py
+++ backend/scripts/__init__.py
@@ -1,4 +1,4 @@
-from .create_deployment import create_deployment_from_template, apply, create_lb_from_template
+from .create_deployment import create_deployment_from_template, apply, create_lb_from_template, get_lb_endpoint
 from .docker_image import build_and_push_image, clone_repo
 from .terraform_parser import create_terraform_files
 from .terraform_parser import check_cluster_change
--- backend/scripts/create_lb.py
+++ backend/scripts/create_lb.py
@@ -1,4 +1,6 @@
 import jinja2
+import os
+import subprocess
 
 def create_lb_from_template(template_file, folder, image_name):
 	templateLoader = jinja2.FileSystemLoader(searchpath="./k8s")
@@ -9,3 +11,9 @@ def create_lb_from_template(template_file, folder, image_name):
 	# to save the results
 	with open(f"{folder}/{template_file}-{image_name}.yaml", "w") as f:
 		f.write(output_text)
+
+def get_lb_endpoint(provider, image_name):
+	# k --context yg-{provider} get svc {image_name}-loadbalancer -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}'
+	response = subprocess.run(["kubectl","--context",f"yg-{provider}", "get" ,"svc" ,f"{image_name}-loadbalancer", "-o=jsonpath='{.status.loadBalancer.ingress[0].hostname}'"], text=True, capture_output=True)
+	output = response.stdout
+	return output
