ce4115366c6f8494e9527e6cc1352c2c62a6feb2
prithvianilk <thepirateking3d2y@gmail.com>
2023-03-25 21:01:36 +0530

    wtf did someone hack my aws

--- backend/scripts/terraform_parser.py
+++ backend/scripts/terraform_parser.py
@@ -1,8 +1,14 @@
 import json
 import os
 import shutil
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 def create_terraform_files(cluster_name,desired_size,instance_type,node_group_name):
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID', default=None)
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY', default=None)
     path=create_temp_folder(cluster_name)
     with open(f"{os.getcwd()}/terraform/terraform.tfvars.json","r") as template_file:
         template_file_data=json.load(template_file)
@@ -18,10 +24,10 @@ def create_terraform_files(cluster_name,desired_size,instance_type,node_group_na
         region="ap-south-1"
         bucket="yg-hack-s3"
         key="{}-terraformstate/terraform.tfstate"
-        access_key = "AKIA3EZCPWK3H2I2TYWH"
-        secret_key = "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"
+        access_key = "{}"
+        secret_key = "{}"
     }}
-}}'''.format(cluster_name)
+}}'''.format(cluster_name, aws_access_key_id, aws_secret_access_key)
     backend_data.write(backend_text)
     create_cluster(path)
 
