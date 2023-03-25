e054c6a947dbb7bf82cbccce35c094c432d30445
compspandan <compspandan@gmail.com>
2023-03-25 18:59:24 +0530

    updates

--- backend/scripts/create_deployment.py
+++ backend/scripts/create_deployment.py
@@ -1,11 +1,11 @@
 import jinja2
 
-def create_deployment_from_template(template_file, provider, cluster_name, image_name):
+def create_deployment_from_template(template_file, folder, image_name):
 	templateLoader = jinja2.FileSystemLoader(searchpath="../k8s")
 	templateEnv = jinja2.Environment(loader=templateLoader)
 	template = templateEnv.get_template(template_file)
 
 	output_text = template.render({"image_name": image_name})  # this is where to put args to the template renderer
 	# to save the results
-	with open(f"../temp/{provider}-{cluster_name}/{template_file}-{image_name}.yaml", "w") as f:
+	with open(f"{folder}/{template_file}-{image_name}.yaml", "w") as f:
 		f.write(output_text)
