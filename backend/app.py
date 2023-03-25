cadce15cc402e5988bae241f75f967e0eb176cd2
compspandan <compspandan@gmail.com>
2023-03-25 21:35:10 +0530

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from scripts import check_cluster_change
 from scripts import build_and_push_image, clone_repo
 from scripts import create_deployment_from_template, apply, create_lb_from_template
@@ -30,11 +30,10 @@ def submit():
 	data = request.get_json()
 	check_cluster_change(data)
 	for cluster in data:
+		# creating folder
+		folder = f"./temp/{cluster['provider']}-{cluster['name']}"
+		os.makedirs(folder, mode=511, exist_ok=True)
 		for app in cluster['apps']:
-			# creating folder
-			folder = f"./temp/{cluster['provider']}-{cluster['name']}"
-			os.makedirs(folder, mode=511, exist_ok=True)
-
 			# run scripts here
 			clone_repo(app['app'], app['github_url'], folder)
 			build_and_push_image(app['app'], folder)
@@ -42,8 +41,8 @@ def submit():
 			create_lb_from_template("deployment", folder, app['name'])
 			apply(folder, cluster['provider'])
 
-			# delete folder
-			shutil.rmtree(folder)
+		# delete folder
+		shutil.rmtree(folder)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
